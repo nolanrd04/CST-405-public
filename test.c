d484506c935e434036495ec1dd2a99fccbaf4565
Alpha <jjjoolll13@gmail.com>
2025-10-05 21:35:09 -0700

    Added more TAC optimizations.

--- test.c
+++ test.c
@@ -1,17 +1,12 @@
-int numbers[5];
+int tacOptimizationTest() {
+    int a, b, c;
 
-numbers[0] = 1;
-numbers[1] = 2;
-numbers[2] = numbers[0] + numbers[1];
-print(numbers[2]);
+    a = 5;
+    b = 0;
+    c = a + b;  // Should optimize c=a+b to c=a
+    c = c; // Should eliminate self-assignment
 
-int list[] = {1, 2, 3, 4, 5};
-print(list[3]);
-
-int list2d[3][4];
-
-list2d[0][0] = 6;
-list2d[2][2] = 7;
-
-print(list2d[2][2]);
-print(list2d[0][0]);
+    print(a);
+    print(b);
+    print(c);
+}
